5d9e43af866a0b7d6267e8778cae3a409366e489
ScottCurtis <Scott.Curtis@xero.com>
2024-12-05 09:03:16 +1300

    Day1Part1

--- src/solutions/day1.ts
+++ src/solutions/day1.ts
@@ -1,7 +1,18 @@
 import { Solution } from "./solutions";
 
 export const day1part1: Solution = (input)  => {
-    return input.toUpperCase();
+    const splits = input.split(/\s+/m)
+    const leftListString = splits.filter((_, index) => index % 2 === 0)
+    const rightListString = splits.filter((_, index) => index % 2 === 1)
+    
+    let leftList = leftListString.map(el => Number.parseInt(el))
+    let rightList = rightListString.map(el => Number.parseInt(el))
+    
+    leftList.sort();
+    rightList.sort();
+    
+    const distances = leftList.map((el, index) => Math.abs(el - rightList[index]));
+    return distances.reduce((acc, curr) => acc += curr, 0);
 }
 
 export const day1part2: Solution = (input) => {
