555ad2d49357d844d6d9570975427996da9da71f
ScottCurtis <Scott.Curtis@xero.com>
2024-12-10 12:22:09 +1300

    Day4Part1

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  eslintConfigPrettier,
+];
--- src/solutions/solutions.ts
+++ src/solutions/solutions.ts
@@ -1,6 +1,6 @@
 import {day1part1, day1part2} from "./day1";
 import {day2part1} from "./day2";
-import {day3part1} from "./day3";
+import {day4part1} from "./day4";
 
 export type Solution = (input: string) => string | number;
 
@@ -9,5 +9,5 @@ export const SOLUTIONS: { [key: string]: Solution } = {
     "Day 1 - Part 1": day1part1,
     "Day 1 - Part 2": day1part2,
     "Day 2 - Part 1": day2part1,
-    "Day 3 - Part 1": day3part1
+    "Day 4 - Part 1": day4part1
 }
